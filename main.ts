060c0d2263458aa7dcdbe19d9cef2e511921f122
otariidae <otariidae@users.noreply.github.com>
2021-03-23 02:11:59 +0900

    feat: notify recieved Gmail to Slack channel as text file

---
+++ main.ts
@@ -0,0 +1,55 @@
+const properties = PropertiesService.getScriptProperties()
+const SLACK_TOKEN = properties.getProperty("TOKEN")
+const SLACK_CHANNEL = properties.getProperty("CHANNEL")
+const INVOKE_INTERVAL = 5 * 60 * 1000 // 5 min
+
+function myFunction(): void {
+  const invokeDate = new Date()
+  const afterDate = new Date(invokeDate.getTime() - INVOKE_INTERVAL)
+  const afterInSec = Math.floor(afterDate.getTime() / 1000)
+  // ref: https://developers.google.com/gmail/api/guides/filtering
+  const threads = GmailApp.search(`in:inbox after:${afterInSec}`)
+
+  for (const thread of threads) {
+    const messages = thread.getMessages()
+    for (const message of messages) {
+      const date = message.getDate()
+      if (date < afterDate) {
+        continue
+      }
+
+      const content = `
+Subject: ${message.getSubject()}
+From: ${message.getFrom()}
+To: ${message.getTo()}
+
+${message.getPlainBody()}
+`.trim()
+      // upload mail content as text file
+      // ref: https://api.slack.com/methods/files.upload
+      const payload = {
+        channels: SLACK_CHANNEL,
+        content: content,
+        title: message.getSubject(),
+        filename: message.getHeader("Message-ID"),
+        filetype: "email",
+        initial_comment: thread.getPermalink(),
+      }
+      const httpsResponse = UrlFetchApp.fetch(
+        "https://slack.com/api/files.upload",
+        {
+          headers: {
+            Authorization: `Bearer ${SLACK_TOKEN}`,
+          },
+          method: "post",
+          payload: payload,
+        }
+      )
+      const response = JSON.parse(httpsResponse.getContentText())
+      console.log(response)
+      if (!response.ok) {
+        console.error(`${httpsResponse.getResponseCode()}: ${response.error}`)
+      }
+    }
+  }
+}
